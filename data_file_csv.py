156bc007fbec1ca306e272b7016baa5daaf3d9dd
Your Name <your.email@example.com>
2025-07-13 14:30:27 +0100

    Process Pink Morsels sales and output to data_file.csv

---
+++ data_file_csv.py
@@ -0,0 +1,19 @@
+import csv
+import glob
+
+csv_files = glob.glob("data/*.csv")
+data = []
+
+for filename in csv_files:
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row["product"] == "Pink Morsels":
+                sales = int(row["quantity"] * float(row["price"]))
+                data.append({"Sales": sales, "Date": row["date"], "Region": row["region"]})
+fields = ['Sales', 'Date', 'Region']
+
+with open("data_file.csv", "w", newline='') as outfile:
+    writer = csv.DictWriter(outfile, fieldnames = fields)
+    writer.writeheader()
+    writer.writerows(data)
